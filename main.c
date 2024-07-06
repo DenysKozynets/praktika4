cc641e20f2497637be20901ea211264065aac5a9
Денис Козинец <kn23-d.kozynets@nubip.edu.ua>
2024-07-06 15:06:28 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int count = 0;
+    for (int m = 1; m < n; m++) {
+        if (n % (m + 1) == m) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
